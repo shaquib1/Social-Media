4e7ba0a92d41a5f8abc2ecc55a3b5c0078def2d4
Md Shaquib Alam <shaquib475@gmail.com>
2023-04-18 18:15:25 +0530

    mongodb

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -8,8 +8,15 @@ const morgan =require('morgan')
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true}, ()=>{
+
+console.log("connected ");
+
+});
+
 app.listen(3000,()=>{
 
 console.log("backend is running in background ");
 }
 )
+
